6c2ce55a4ccc09e883d393d8a3a92b4aec15a094
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-05 21:04:14 +0500

    Create SideBar.js

---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './SideBar.css';
+import {
+  FaTwitter, FaFacebookF, FaVine, FaPinterestP,
+} from 'react-icons/fa';
+import { TiSocialGooglePlus } from 'react-icons/ti';
+
+const SideBar = () => {
+  const [style, setStyle] = useState('menu');
+  const [menuStatus, setMenuStatus] = useState('open');
+
+  const handleClick = () => {
+    if (menuStatus === 'open') {
+      setMenuStatus('close');
+      setStyle('menu active');
+    } else if (menuStatus === 'close') {
+      setMenuStatus('open');
+      setStyle('menu');
+    }
+  };
+
+  const navLinks = [
+    { url: '/houses', name: 'Houses' },
+    { url: '/reserve', name: 'Reserve' },
+    { url: '/my_reservations', name: 'My Reservations' },
+    { url: '/add_house', name: 'Add House' },
+    { url: '/delete_house', name: 'Delete House' },
+  ];
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>
+        menu
+      </button>
+      <div className={style}>
+        <div className="logo">Homely</div>
+        <ul>
+          {navLinks.map(({ url, name }) => (
+            <li key={name}>
+              <a href={url}>{name}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="footer">
+          <div className="social">
+            <FaTwitter />
+            <FaFacebookF />
+            <TiSocialGooglePlus />
+            <FaVine />
+            <FaPinterestP />
+          </div>
+          <small>&copy; 2023 FEEL AT HOME</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
