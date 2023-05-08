f4a9d9e1fd386814b3c1faf26a64c0adb05f5aac
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-08 20:41:27 +0500

    Update Houses.js
    Remove mock data from the component.

--- src/components/houses/Houses.js
+++ src/components/houses/Houses.js
@@ -9,35 +9,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import styles from './Houses.scss';
+import { houseData } from '../../mockData';
 
-export const houseData = [
-  {
-    id: 1,
-    name: 'House 1',
-    image: 'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    description: 'Yellow Concrete House.',
-  },
-  {
-    id: 2,
-    name: 'House 2',
-    image: 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    description: 'White and Red Wooden House With Fence.',
-  },
-  {
-    id: 3,
-    name: 'House 3',
-    image: 'https://images.pexels.com/photos/2587054/pexels-photo-2587054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    description: 'Beautiful House in Australia.',
-  },
-  {
-    id: 4,
-    name: 'House 4',
-    image: 'https://images.pexels.com/photos/2587054/pexels-photo-2587054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    description: 'Beautiful House in Australia. One of the best places you can find here in Australia.',
-  },
-];
-
-export const Houses = () => (
+const Houses = () => (
   <section>
     <h2>LATEST HOUSES</h2>
     <h3 className="email">Please select your Dream House</h3>
@@ -64,3 +38,5 @@ export const Houses = () => (
     </Swiper>
   </section>
 );
+
+export default Houses;
