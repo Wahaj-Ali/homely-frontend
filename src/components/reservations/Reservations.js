f6c9be015076a3e2370a22887b5616a5c5d98ddc
blase147 <solarmails2@gmail.com>
2023-05-09 00:32:55 +0100

    Fix linters

--- src/components/reservations/Reservations.js
+++ src/components/reservations/Reservations.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReservationsData } from '../../mockData';
+import './Reservations.scss';
 
 const Reservations = () => (
   <section>
-    <div className="my-reservationsContainer">
+    <div className="myReservationsContainer">
       {
         ReservationsData.map((item) => (
           <div className="list-items" key={item.id}>
