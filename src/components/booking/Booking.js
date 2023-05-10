682406436861162c9c0b33169c080dd8f3585901
blase147 <solarmails2@gmail.com>
2023-05-10 10:45:32 +0100

    Add booking

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -23,13 +23,45 @@ const Booking = () => (
       create a peaceful oasis.
     </p>
 
-    <div className="buttonContainer">
-      <button type="button" className="locationButton">Location</button>
-      <button type="button" className="reserveButton">Reserve Now</button>
-    </div>
+    <>
+      {status === 'succeeded' ? (
+        <form onSubmit={handleSubmit}>
+          <DatePicker
+            value={startDate}
+            onChange={setStartDate}
+            format="DD/MM/YYYY"
+            placeholder="Choose Start Date"
+          />
 
+          <DatePicker
+            value={endDate}
+            onChange={setEndDate}
+            format="DD/MM/YYYY"
+            placeholder="Choose End Date"
+          />
+
+          <select value={selectedValue} onChange={handleSelectChange}>
+            {packages.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.city}
+                {item.date}
+              </option>
+            ))}
+          </select>
+
+          <br />
+
+          <input type="submit" value="Submit" />
+        </form>
+      ) : <img src={loadingGif} alt="loading" /> }
+
+      <Link to="/">
+        Go Back
+      </Link>
+    </>
   </div>
 
 );
+
 
 export default Booking;
