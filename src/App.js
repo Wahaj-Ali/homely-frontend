90d56b2fcc3a97c57ff99bc263ce94210f1295a9
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-11 21:21:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {
   BrowserRouter as Router, Routes, Route, Outlet,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 import './App.css';
 import SideBar from './components/sidebar/SideBar';
 import HousesDetails from './components/houses details/HouseDetails';
@@ -17,15 +19,17 @@ const SidebarLayout = () => (
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<SidebarLayout />}>
-            <Route path="/" element={<Houses />} />
-            <Route path="/:id" element={<HousesDetails />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<SidebarLayout />}>
+              <Route path="/" element={<Houses />} />
+              <Route path="/:id" element={<HousesDetails />} />
+            </Route>
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
