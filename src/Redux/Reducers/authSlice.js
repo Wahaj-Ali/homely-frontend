e79a9249739f0d351fef0a1f0f737c3739550b42
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-12 02:14:04 +0500

    Update authSlice.js
    Add action to log out the current user.

--- src/Redux/Reducers/authSlice.js
+++ src/Redux/Reducers/authSlice.js
@@ -5,14 +5,37 @@ export const login = createAsyncThunk('auth/login', async ({ email, password }) 
   const response = await axios.post('http://localhost:4000/login', {
     user: { email, password },
   });
-
+  // console.log(response);
   if (response.status >= 200 && response.status < 300) {
-    const token = response.headers.get('Authorization');
+    // const token = response.headers.get('Authorization');
+    const authorizationHeader = response.headers.authorization;
+    const token = authorizationHeader ? authorizationHeader.split(' ')[1] : null;
     localStorage.setItem('token', token);
     return token;
   }
   throw new Error(response);
 });
+
+export const logoutUser = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.delete('http://localhost:4000/logout', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+
+      if (response.status === 200) {
+        localStorage.removeItem('token');
+        return { success: true };
+      }
+      return thunkAPI.rejectWithValue('Logout failed');
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
 
 const authSlice = createSlice({
   name: 'auth',
