8d23eae87e8e76022cc86a791276327ef099514e
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-11 19:55:53 +0500

    Update App.js
    Update the routes so that the sidebar is only available for the components it is required for excluding the log in page.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import {
-  BrowserRouter as Router, Routes, Route,
+  BrowserRouter as Router, Routes, Route, Outlet,
 } from 'react-router-dom';
 import './App.css';
 import SideBar from './components/sidebar/SideBar';
 import HousesDetails from './components/houses details/HouseDetails';
 import Houses from './components/houses/Houses';
+import Login from './components/auth/Login';
+
+const SidebarLayout = () => (
+  <>
+    <SideBar />
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <SideBar />
         <Routes>
-          <Route path="/" element={<Houses />} />
-          <Route path="/:id" element={<HousesDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<SidebarLayout />}>
+            <Route path="/" element={<Houses />} />
+            <Route path="/:id" element={<HousesDetails />} />
+          </Route>
         </Routes>
       </Router>
     </div>
