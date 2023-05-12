97901b329cf743aec702be7ccb60a1ae45f359e2
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-12 18:57:11 +0500

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Reducers/authSlice';
+import houseReducer from './Reducers/houseSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    houses: houseReducer,
   },
 });
 
