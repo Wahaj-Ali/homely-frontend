1f6b3f9ed3364f2a5cce7925fb10a94b92a21030
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-12 19:39:21 +0500

    Update Houses.js
    Show loading when data is being fetched from the API.

--- src/components/houses/Houses.js
+++ src/components/houses/Houses.js
@@ -28,26 +28,29 @@ const Houses = () => {
       <h2>LATEST HOUSES</h2>
       <h3 className="email">Please select your Dream House</h3>
       <div className="points">.........................</div>
-      <Swiper
-        className="house-list"
-        modules={[Navigation, A11y]}
-        spaceBetween={10}
-        slidesPerView={3}
-        navigation
-      >
-        {
-          houses.map((item) => (
-            <SwiperSlide className="list-items" key={item.id}>
-              <div className="houses-imgs">
-                <img src={item.image} style={{ width: '70', height: '70' }} alt={item.name} />
-              </div>
-              <h5 className={styles.name}><Link to={`${item.id}`}>{item.name}</Link></h5>
-              <div className="points">....................</div>
-              <p className="email">{item.description}</p>
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'succeeded' && (
+        <Swiper
+          className="house-list"
+          modules={[Navigation, A11y]}
+          spaceBetween={10}
+          slidesPerView={3}
+          navigation
+        >
+          {
+            houses.map((item) => (
+              <SwiperSlide className="list-items" key={item.id}>
+                <div className="houses-imgs">
+                  <img src={item.image} style={{ width: '70', height: '70' }} alt={item.name} />
+                </div>
+                <h5 className={styles.name}><Link to={`${item.id}`}>{item.name}</Link></h5>
+                <div className="points">....................</div>
+                <p className="email">{item.description}</p>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      )}
     </section>
   );
 };
