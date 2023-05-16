5ce6fc6d23287c914fb9c033c34b2e1476a59442
blase147 <solarmails2@gmail.com>
2023-05-16 12:30:29 +0100

    FIX linter error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import {
-  BrowserRouter as Router, Routes, Route, Outlet,
+  BrowserRouter as Router, Routes, Route, 
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import './App.css';
-import SideBar from './components/sidebar/SideBar';
 import HousesDetails from './components/houses details/HouseDetails';
 import Houses from './components/houses/Houses';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import AddHouse from './components/add hosue/addHouse';
 import Reservations from './components/Reservations/Reservations';
 import Booking from './components/booking/Booking';
 import DeleteHouse from './components/DeleteHouse/DeleteHouse';
-
-const SidebarLayout = () => (
-  <>
-    <SideBar />
-    <Outlet />
-  </>
-);
 
 function App() {
   return (
@@ -27,13 +16,8 @@ function App() {
       <Provider store={store}>
         <Router>
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/add_house" element={<AddHouse />} />
-            <Route element={<SidebarLayout />}>
-              <Route path="/" element={<Houses />} />
-              <Route path="/:id" element={<HousesDetails />} />
-            </Route>
+            <Route path="/" element={<Houses />} />
+            <Route path="/:id" element={<HousesDetails />} />
             <Route path="my_reservations" element={<Reservations />} />
             <Route path="reserve" element={<Booking />} />
             <Route path="delete_house" element={<DeleteHouse />} />
