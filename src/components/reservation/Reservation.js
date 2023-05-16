dbe240d0c39b4c9d55d3fcccb8c83879d99b78f8
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-17 04:58:02 +0500

    Update Reservation.js

--- src/components/reservation/Reservation.js
+++ src/components/reservation/Reservation.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { IoCaretBackOutline } from 'react-icons/io5';
 import { createReservation } from '../../Redux/Reducers/reservationsSlice';
 import { fetchCurrentUser } from '../../Redux/Reducers/authSlice';
+import './Reservation.scss';
 
 const Reservation = () => {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -39,28 +39,43 @@ const Reservation = () => {
   };
 
   return (
-    <section>
-      <div>
-        <h2>
-          Book your visit to
-          {' '}
-          {house ? house.name : 'Loading...'}
-        </h2>
-        <hr />
-      </div>
-      <div>
-        <p>Please Select a date of your visit.</p>
-        <div>
-          <DatePicker selected={selectedDate} onChange={handleDateChange} />
+    <section
+      className="reservation_sec"
+      style={{ backgroundImage: `linear-gradient(rgb(133 209 25 / 60%), rgb(140 255 0 / 62%)), url(${house.image})` }}
+    >
+      {house ? (
+        <div className="reservation">
+          <div>
+            <h2 className="title">
+              Book your visit to
+              {' '}
+              {house.name}
+            </h2>
+            <hr />
+          </div>
+          <div>
+            <p>Please Select a date of your visit.</p>
+            <div>
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleDateChange}
+                format="DD/MM/YYYY"
+                placeholder="Choose Start Date"
+              />
+            </div>
+          </div>
+          <button type="button" className="book-btn" onClick={handleCreateReservation}>
+            Confirm Visit
+          </button>
+          <Link to={`/${house.id}`}>
+            <button type="button" aria-label="Go back">
+              <IoCaretBackOutline />
+            </button>
+          </Link>
         </div>
-      </div>
-
-      <button type="button" className="book-btn" onClick={handleCreateReservation}>
-        Confrim Visit
-      </button>
-      <Link to={`/${house.id}`}>
-        <button type="button" aria-label="Go back"><IoCaretBackOutline /></button>
-      </Link>
+      ) : (
+        <p>Please go to the main page to select your desired house.</p>
+      )}
     </section>
   );
 };
