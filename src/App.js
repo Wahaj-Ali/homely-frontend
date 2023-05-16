09c8cfa6354b2f1a97bd67eb2d907086386657c9
blase147 <solarmails2@gmail.com>
2023-05-16 13:52:02 +0100

    FIX linter error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import {
   BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Redux/store';
 import './App.css';
+import { Provider } from 'react-redux';
+import SideBar from './components/sidebar/SideBar';
 import HousesDetails from './components/houses details/HouseDetails';
 import Houses from './components/houses/Houses';
 import Reservations from './components/Reservations/Reservations';
 import Booking from './components/booking/Booking';
-import DeleteHouse from './components/DeleteHouse/DeleteHouse';
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
+      <Provider>
         <Router>
+          <SideBar />
           <Routes>
             <Route path="/" element={<Houses />} />
             <Route path="/:id" element={<HousesDetails />} />
-            <Route path="my_reservations" element={<Reservations />} />
-            <Route path="reserve" element={<Booking />} />
-            <Route path="delete_house" element={<DeleteHouse />} />
+            <Route path="/Reservations" element={<Reservations />} />
+            <Route path="/Booking" element={<Booking />} />
           </Routes>
         </Router>
       </Provider>
