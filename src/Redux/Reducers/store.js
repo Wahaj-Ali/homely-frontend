cb86663a18db6a4ca76b0176dec8d70195b6ab11
blase147 <solarmails2@gmail.com>
2023-05-16 09:41:04 +0100

    Add reservation, deleteHouse reducers

--- src/Redux/Reducers/store.js
+++ src/Redux/Reducers/store.js
@@ -4,6 +4,9 @@ import housesReducer from './Reducers/houseSlice';
 import houseReducer from './Reducers/houseDetailsSlice';
 import signUpReducer from './Reducers/regSlice';
 import addHouseReducer from './Reducers/addHouseSlice';
+import deleteHouseReducer from './Reducers/deleteHouseSlice';
+import reservationReducer from './Reducers/reservationSlice';
+import reservationsReducer from './Reducers/reservationsSlice';
 
 const store = configureStore({
   reducer: {
@@ -12,6 +15,9 @@ const store = configureStore({
     houses: housesReducer,
     houseDetail: houseReducer,
     addHouse: addHouseReducer,
+    deleteHouse: deleteHouseReducer,
+    reservation: reservationReducer,
+    reservations: reservationsReducer,
   },
 });
 
