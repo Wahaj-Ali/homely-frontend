1eb7b1951277a32f14f3fc14fc090047da3a5507
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-17 15:21:22 +0500

    Update MyReservations.js

--- src/components/my reservations/MyReservations.js
+++ src/components/my reservations/MyReservations.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../../Redux/Reducers/reservationsSlice';
 
-const MyReservations = () => (
-  <div>MyReservations</div>
-);
+const ReservationList = () => {
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.auth.id);
+  const reservations = useSelector((state) => state.reservations.reservations);
+  const loading = useSelector((state) => state.reservations.loading);
+  const error = useSelector((state) => state.reservations.error);
 
-export default MyReservations;
+  useEffect(() => {
+    dispatch(fetchReservations(userId));
+  }, [dispatch, userId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>Your Reservations</h2>
+      {reservations.map((reservation) => (
+        <div key={reservation.id}>
+          <p>
+            Visit Date:
+            {reservation.reservation_date}
+          </p>
+          <p>
+            House ID:
+            {reservation.house_id}
+          </p>
+          <p>
+            House Name:
+            {reservation.house_name}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ReservationList;
