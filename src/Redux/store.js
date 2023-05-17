540260c1c6c88288f35534ff89dbd9459d92c8d1
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-05-17 20:29:00 +0500

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,7 +4,6 @@ import housesReducer from './Reducers/houseSlice';
 import houseReducer from './Reducers/houseDetailsSlice';
 import signUpReducer from './Reducers/regSlice';
 import addHouseReducer from './Reducers/addHouseSlice';
-import deleteHouseReducer from './Reducers/deleteHouseSlice';
 
 const store = configureStore({
   reducer: {
@@ -13,7 +12,6 @@ const store = configureStore({
     houses: housesReducer,
     houseDetail: houseReducer,
     addHouse: addHouseReducer,
-    deleteHouse: deleteHouseReducer,
   },
 });
 
